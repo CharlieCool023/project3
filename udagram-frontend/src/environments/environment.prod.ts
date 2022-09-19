8bda7dee93ba9958dea21303e5e7de83d2688579
charles onah <charlesonah023@gmail.com>
2022-09-19 10:38:29 +0100

    project 3

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http:/a18256fe4ca93491a6eb6e1610dd49d-1393539:8080/api/v0'
 };
 
 /*
